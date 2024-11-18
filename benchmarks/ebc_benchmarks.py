3fd82e80ee2997e060a5a4d55d202e5a84b115cc
Xuting ZHOU <xutingz@xutingz-mlt.client.nvidia.com>
2024-11-18 14:01:29 +0800

    fix

--- benchmarks/ebc_benchmarks.py
+++ benchmarks/ebc_benchmarks.py
@@ -21,7 +21,7 @@ from torchrec.modules.fused_embedding_modules import FusedEmbeddingBagCollection
 
 # Reference: https://github.com/facebookresearch/dlrm/blob/main/torchrec_dlrm/README.MD
 DLRM_NUM_EMBEDDINGS_PER_FEATURE = [
-    1000000,
+    5000000,
 ]
 
 
