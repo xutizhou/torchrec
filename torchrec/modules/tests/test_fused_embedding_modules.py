74953c232b701ad1d18131eb76c32ec13cdfb4af
xutingz <xutingz@nvidia.com>
2024-11-25 12:05:02 +0800

    fix

--- torchrec/modules/tests/test_fused_embedding_modules.py
+++ torchrec/modules/tests/test_fused_embedding_modules.py
@@ -1037,26 +1037,28 @@ class FusedEmbeddingCollectionTest(unittest.TestCase):
 
         for epoch in range(num_epochs):
             for step in range(num_steps):
+                torch.cuda.nvtx.range_push("FEC Dataloader Pass")
+                features = dataset.__getitem__(step)
+                features = features.to(device)
+                torch.cuda.nvtx.range_pop() 
+                torch.cuda.nvtx.range_push("FEC Forward Pass")
+                fused_embeddings = fused_ec(features)
+                torch.cuda.nvtx.range_pop() 
+                fused_vals = []
                 if epoch==1 and step==1:
-                    with profile(
-                        activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],  # Profile both CPU and GPU
-                        record_shapes=True,  # Record tensor shapes
-                        with_stack=True      # Include stack traces for detailed analysis
-                    ) as prof:                    
-                        torch.cuda.nvtx.range_push("FEC Dataloader Pass")
-                        features = dataset.__getitem__(step)
-                        features = features.to(device)
-                        torch.cuda.nvtx.range_pop() 
-                        torch.cuda.nvtx.range_push("FEC Forward Pass")
-                        fused_embeddings = fused_ec(features)
-                        torch.cuda.nvtx.range_pop() 
-                        fused_vals = []
-                        for _name, jt in fused_embeddings.items():
-                            fused_vals.extend(jt.to_dense())
-                        torch.cuda.nvtx.range_push("FEC Backward + Gradient Pass")
-                        torch.cat(fused_vals).sum().backward()
-                        torch.cuda.nvtx.range_pop() 
-                        prof.export_chrome_trace("trace.json")
+                    prof = profile(
+                            activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],  # Profile both CPU and GPU
+                            record_shapes=True,  # Record tensor shapes
+                            with_stack=True      # Include stack traces for detailed analysis
+                        )
+                for _name, jt in fused_embeddings.items():
+                    fused_vals.extend(jt.to_dense())
+                torch.cuda.nvtx.range_push("FEC Backward + Gradient Pass")
+                torch.cat(fused_vals).sum().backward()
+                torch.cuda.nvtx.range_pop() 
+                if epoch==1 and step==1:
+                    break
+        prof.export_chrome_trace("trace.json")
 
 
         end_time = time.perf_counter()
