cc221f5fa34c355f4fc0a888fed2e7b3d17cd90e
xutingz <xutingz@nvidia.com>
2024-12-10 00:08:44 +0800

    print

--- torchrec/distributed/composable/tests/benchmark.py
+++ torchrec/distributed/composable/tests/benchmark.py
@@ -104,7 +104,7 @@ def _test_sharding(  # noqa C901
     use_index_dedup: bool = False,
 ) -> None:
     trec_dist.comm_ops.set_gradient_division(False)
-    dataset = CustomDataset(10, 10, batch_size=10, seq_len=10, device=torch.device("cuda"))
+    dataset = CustomDataset(num_steps=num_steps, hash_size=hash_size, batch_size=batch_size, seq_len=seq_len, device=torch.device("cuda"))
     with MultiProcessContext(rank, world_size, backend, local_size) as ctx:
         print(f"###########ctx.device: {ctx.device}")
         sharder = EmbeddingCollectionSharder(use_index_dedup=use_index_dedup)
@@ -213,7 +213,7 @@ def _test_sharding(  # noqa C901
         sharded_model_pred_jts_dict = sharded_model(indices)
         print(sharded_model_pred_jts_dict['feature_0'].values())
 
-        for step in range(10):
+        for step in range(num_steps):
             # torch.cuda.nvtx.range_push("FEC Dataloader Pass")
             features = dataset.__getitem__(step)
             features = features.to(ctx.device)
@@ -247,8 +247,8 @@ class ShardedEmbeddingCollectionParallelTest(MultiProcessTestBase):
             EmbeddingConfig(
                 name="table_0",
                 feature_names=["feature_0"],
-                embedding_dim=128,
-                num_embeddings=400,
+                embedding_dim=embedding_dim,
+                num_embeddings=hash_size,
             ),
         ]
 
