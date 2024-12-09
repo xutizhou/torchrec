b4dcfdb2d5b6e27648fc6179b10985b14440828f
xutingz <xutingz@nvidia.com>
2024-12-10 00:25:51 +0800

    print

--- torchrec/distributed/composable/tests/benchmark.py
+++ torchrec/distributed/composable/tests/benchmark.py
@@ -42,22 +42,6 @@ from torchrec.modules.embedding_modules import EmbeddingCollection
 from torchrec.sparse.jagged_tensor import JaggedTensor, KeyedJaggedTensor
 from torchrec.test_utils import skip_if_asan_class
 
-hash_size = 8000000
-embedding_dim = 128
-batch_size = 2048
-seq_len = 4096
-num_epochs = 1
-dataset_size = 8000000000
-num_steps = dataset_size // (batch_size * seq_len)
-print(f"hash_size: {hash_size}")
-print(f'hash_size GB: {hash_size * embedding_dim * 4 / 1024 / 1024 / 1024}')
-print(f"embedding_dim: {embedding_dim}")
-print(f"batch_size: {batch_size}")
-print(f"seq_len: {seq_len}")
-print(f"dataset_size: {dataset_size}")
-print(f"fetched embedding size GB: {dataset_size * embedding_dim * 4 / 1024 / 1024 / 1024}")
-print(f"num_epochs: {num_epochs}")
-print(f"num_steps: {num_steps}")
 class CustomDataset():
     def __init__(self, num_steps, hash_size, batch_size, seq_len, device):
         self.num_steps = num_steps
@@ -104,7 +88,7 @@ def _test_sharding(  # noqa C901
     use_index_dedup: bool = False,
 ) -> None:
     trec_dist.comm_ops.set_gradient_division(False)
-    # dataset = CustomDataset(num_steps=num_steps, hash_size=hash_size, batch_size=batch_size, seq_len=seq_len, device=torch.device("cuda"))
+    dataset = CustomDataset(10, 10, batch_size=10, seq_len=10, device=torch.device("cuda"))
     with MultiProcessContext(rank, world_size, backend, local_size) as ctx:
         print(f"###########ctx.device: {ctx.device}")
         sharder = EmbeddingCollectionSharder(use_index_dedup=use_index_dedup)
@@ -213,14 +197,14 @@ def _test_sharding(  # noqa C901
         sharded_model_pred_jts_dict = sharded_model(indices)
         print(sharded_model_pred_jts_dict['feature_0'].values())
 
-        # for step in range(num_steps):
-        #     # torch.cuda.nvtx.range_push("FEC Dataloader Pass")
-        #     features = dataset.__getitem__(step)
-        #     features = features.to(ctx.device)
-        #     # torch.cuda.nvtx.range_pop() 
-        #     # torch.cuda.nvtx.range_push("FEC Forward Pass")
-        #     fused_embeddings = sharded_model(features)   
-        #     print(f"embeddings are {fused_embeddings['feature_0'].values()}")     
+        for step in range(10):
+            # torch.cuda.nvtx.range_push("FEC Dataloader Pass")
+            features = dataset.__getitem__(step)
+            features = features.to(ctx.device)
+            # torch.cuda.nvtx.range_pop() 
+            # torch.cuda.nvtx.range_push("FEC Forward Pass")
+            fused_embeddings = sharded_model(features)   
+            print(f"embeddings are {fused_embeddings['feature_0'].values()}")     
 
 
 @skip_if_asan_class
@@ -247,8 +231,8 @@ class ShardedEmbeddingCollectionParallelTest(MultiProcessTestBase):
             EmbeddingConfig(
                 name="table_0",
                 feature_names=["feature_0"],
-                embedding_dim=embedding_dim,
-                num_embeddings=hash_size,
+                embedding_dim=128,
+                num_embeddings=400,
             ),
         ]
 
