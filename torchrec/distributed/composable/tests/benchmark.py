5cee07554efeeafefe001da0e2ba4d07747bbfb8
xutingz <xutingz@nvidia.com>
2024-12-10 13:06:06 +0800

    pass example

--- torchrec/distributed/composable/tests/benchmark.py
+++ torchrec/distributed/composable/tests/benchmark.py
@@ -110,7 +110,9 @@ def _test_sharding(  # noqa C901
     use_index_dedup: bool = False,
 ) -> None:
     trec_dist.comm_ops.set_gradient_division(False)
+    print_gpu_memory_usage()   
     dataset = CustomDataset(num_steps=num_steps, hash_size=hash_size, batch_size=batch_size, seq_len=seq_len, device=torch.device("cuda"))
+    print_gpu_memory_usage()   
     with MultiProcessContext(rank, world_size, backend, local_size) as ctx:
         print(f"###########ctx.device: {ctx.device}")
         sharder = EmbeddingCollectionSharder(use_index_dedup=use_index_dedup)
