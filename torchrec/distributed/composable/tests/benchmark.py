84278b458b23e48756e1417d8164ef6e0a326141
xutingz <xutingz@nvidia.com>
2024-12-11 16:03:01 +0800

    nsys

--- torchrec/distributed/composable/tests/benchmark.py
+++ torchrec/distributed/composable/tests/benchmark.py
@@ -185,17 +185,16 @@ def _test_sharding(  # noqa C901
 
         import time
         train_start_time = time.perf_counter()
-        features = dataset.__getitem__(1)
         for step in range(num_steps):
             # torch.cuda.nvtx.range_push("FEC Dataloader Pass")
-            # features = dataset.__getitem__(step)
+            features = dataset.__getitem__(step)
             # print(f"features at rank:{features.values().get_device()}current GPU is{ctx.device}")  
-            # features = features.to(ctx.device)
+            features = features.to(ctx.device)
             # print(f"features at rank:{features.values().get_device()}current GPU is{ctx.device}") 
             # torch.cuda.nvtx.range_pop() 
-            # torch.cuda.nvtx.range_push("FEC Forward Pass")
+            torch.cuda.nvtx.range_push("FEC Forward Pass")
             fused_embeddings = sharded_model(features)  
-            # torch.cuda.nvtx.range_pop() 
+            torch.cuda.nvtx.range_pop() 
             # print(f"embeddings are {fused_embeddings['feature_0'].values()}")  
         train_end_time = time.perf_counter()
         train_time = train_end_time - train_start_time
